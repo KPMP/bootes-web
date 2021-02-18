eba54548cb9beac4b9bacc91dbf015be4b690a70
Becky Reamy <rlreamy@umich.edu>
2021-02-18 16:46:53 -0500

    KPMP-2369: Added missing polyfills

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'react-app-polyfill/ie11';
 import 'core-js/features/array/find';
-import 'core-js/features/array/includes';
+import 'core-js/features/array/find-index';
+import 'core-js/features/object/assign';
 import 'core-js/features/number/is-nan';
+import 'core-js/features/array/includes';
+import 'core-js/features/string/includes';
 import '../node_modules/bootstrap-css-only/css/bootstrap.min.css';
 import './index.css';
 import './react-table.css';
