69d625f50cf6292a71ef9d205dd687eb39641956
Becky Reamy <rlreamy@umich.edu>
2021-02-18 16:10:15 -0500

    KPMP-2369: change polyfills

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '@babel/polyfill';
-import 'react-app-polyfill/stable';
+import 'react-app-polyfill/ie11';
 import 'core-js/features/array/find';
+import 'core-js/features/array/includes';
+import 'core-js/features/number/is-nan';
 import '../node_modules/bootstrap-css-only/css/bootstrap.min.css';
 import './index.css';
 import './react-table.css';
